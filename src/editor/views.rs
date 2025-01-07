74e2117cb20c725ad45b19319a781d9162112060
Ben Meier <1651305+astromechza@users.noreply.github.com>
2025-01-07 17:10:59 +0000

    feat: post creation and editing (#3)
    * successful post create and edit urls
    
    * feat: added debug handler
    
    * feat: added slug deduplication
    
    * feat: added label and published config
    
    * fix: lint (#4)
    
    * fix: lint
    
    * chore: allow unused methods

---
+++ src/editor/views.rs
@@ -0,0 +1,252 @@
+use axum::http::{HeaderMap, HeaderValue, Method, StatusCode, Uri};
+use axum::response::{IntoResponse, Response};
+use maud::{html, Markup, DOCTYPE};
+use object_store::ObjectMeta;
+use crate::htmx::HtmxContext;
+use crate::store::Post;
+use crate::store::PostContentType::{Markdown, RestructuredText};
+
+fn render_body_html(title: impl AsRef<str>, inner: Markup) -> Markup {
+    html! {
+        (DOCTYPE)
+        html {
+            head {
+                title { (title.as_ref()) }
+                style {
+                    r#"
+                        /**
+                         * Minified by jsDelivr using clean-css v5.3.2.
+                         * Original file: /npm/modern-normalize@3.0.1/modern-normalize.css
+                         *
+                         * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
+                         */
+                        /*! modern-normalize v3.0.1 | MIT License | https://github.com/sindresorhus/modern-normalize */
+                        *,::after,::before{box-sizing:border-box}html{font-family:system-ui,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji';line-height:1.15;-webkit-text-size-adjust:100%;tab-size:4}body{margin:0}b,strong{font-weight:bolder}code,kbd,pre,samp{font-family:ui-monospace,SFMono-Regular,Consolas,'Liberation Mono',Menlo,monospace;font-size:1em}small{font-size:80%}sub,sup{font-size:75%;line-height:0;position:relative;vertical-align:baseline}sub{bottom:-.25em}sup{top:-.5em}table{border-color:currentcolor}button,input,optgroup,select,textarea{font-family:inherit;font-size:100%;line-height:1.15;margin:0}[type=button],[type=reset],[type=submit],button{-webkit-appearance:button}legend{padding:0}progress{vertical-align:baseline}::-webkit-inner-spin-button,::-webkit-outer-spin-button{height:auto}[type=search]{-webkit-appearance:textfield;outline-offset:-2px}::-webkit-search-decoration{-webkit-appearance:none}::-webkit-file-upload-button{-webkit-appearance:button;font:inherit}summary{display:list-item}
+                        /*# sourceMappingURL=/sm/d2d8cd206fb9f42f071e97460f3ad9c875edb5e7a4b10f900a83cdf8401c53a9.map */
+                    "#
+                }
+                link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.min.css" integrity="sha512-xiunq9hpKsIcz42zt0o2vCo34xV0j6Ny8hgEylN3XBglZDtTZ2nwnqF/Z/TTCc18sGdvCjbFInNd++6q3J0N6g==" crossorigin="anonymous" referrerpolicy="no-referrer";
+                link rel="shortcut icon" type="image/svg" href="/favicon.svg";
+                script src="https://cdnjs.cloudflare.com/ajax/libs/htmx/2.0.4/htmx.min.js" integrity="sha512-2kIcAizYXhIn8TzUvqzEDZNuDZ+aW7yE/+f1HJHXFjQcGNfv1kqzJSTBRBSlOgp6B/KZsz1K0a3ZTqP9dnxioQ==" crossorigin="anonymous" referrerpolicy="no-referrer" {};
+            }
+            body hx-boost="true" id="body" {
+                (inner)
+            }
+        }
+    }
+}
+
+pub(crate) fn render_body_semantics(header: &str, sections: Vec<Markup>) -> Markup {
+    html! {
+        main class="container" {
+            header {
+                nav {
+                    a href="/" { "home "}
+                    " | "
+                    a href="/posts" { "posts" }
+                    " | "
+                    a href="/images" { "images" }
+                    " | "
+                    a href="/debug" { "debug" }
+                }
+                h2 { (header) }
+            }
+            @for section in sections {
+                section { (section) }
+            }
+        }
+    }
+}
+
+/// Renders either the whole main html, or returns just the content suitable for swapping into the main element.
+pub(crate) fn render_body_html_or_htmx(code: StatusCode, title: impl AsRef<str>, inner: Markup, htmx_context: Option<HtmxContext>) -> Response {
+    let mut hm = HeaderMap::new();
+    hm.insert("Content-Type", HeaderValue::from_static("text/html"));
+    hm.insert("Vary", HeaderValue::from_static("HX-Request"));
+    if let Some(hc) = htmx_context {
+        // Ensure that we retarget the request if it's attempting to swap to the wrong place.
+        if hc.target.is_some_and(|x| x.ne("#body")) {
+            hm.insert("HX-Retarget", HeaderValue::from_static("#body"));
+            hm.insert("HX-Reswap", HeaderValue::from_static("innerHTML"));
+        }
+        // HTMX requires HTTP 200 responses by default.
+        (StatusCode::OK, hm, html! {
+            title { (title.as_ref()) }
+            (inner)
+        }.0).into_response()
+    } else {
+        (code, hm, render_body_html(title, inner).0).into_response()
+    }
+}
+
+pub(crate) fn internal_error_page(err: anyhow::Error, htmx_context: Option<HtmxContext>) -> Response {
+    render_body_html_or_htmx(StatusCode::INTERNAL_SERVER_ERROR, "Internal Error", render_body_semantics("Internal Error", vec![html! {
+        p {
+            "An internal error has occurred. Please navigate back using the links above."
+        }
+        code {
+            (err)
+        }
+    }]), htmx_context)
+}
+
+pub(crate) fn not_found_page(method: Method, uri: Uri, htmx_context: Option<HtmxContext>) -> Response {
+    render_body_html_or_htmx(StatusCode::NOT_FOUND, "Not Found", render_body_semantics("Not Found", vec![html! {
+        p {
+            code { (method.as_str()) }
+            " "
+            code { (uri.path()) }
+            " not found"
+        }
+    }]), htmx_context)
+}
+
+pub(crate) fn list_posts_page(posts: Vec<Post>, htmx_context: Option<HtmxContext>) -> Response {
+    render_body_html_or_htmx(StatusCode::OK, "Posts", render_body_semantics("Posts", vec![html! {
+
+        a href="/posts/new" class="button" {
+            "New Post"
+        }
+
+        table {
+            thead {
+                tr {
+                    th { "Date" }
+                    th { "Slug" }
+                    th { "Title" }
+                    th { "Published" }
+                    th { "Labels" }
+                }
+            }
+            tbody {
+                @if posts.is_empty() {
+                    tr {
+                        td colspan="5" { "No posts, please create one" }
+                    }
+                } else {
+                    @for post in posts {
+                        tr {
+                            td {
+                                a href={"/posts/" (post.slug)} {
+                                    (post.date)
+                                }
+                            }
+                            td { (post.slug) }
+                            td { (post.title) }
+                            td { (post.published) }
+                            td { (post.labels.join(", ")) }
+                        }
+                    }
+                }
+            }
+        }
+    }]), htmx_context)
+}
+
+fn render_post_form(current: Option<(Post, String)>, is_new: bool) -> Markup {
+    html! {
+        div.row {
+            div.column {
+                label for="slug" { "URL Slug (immutable)" }
+                @if is_new {
+                    input type="text" name="slug" spellcheck="true" required="true" placeholder="the-url-slug-of-this-post" value=[current.as_ref().map(|x| &x.0.slug)];
+                } @else {
+                    input type="text" disabled? value=[current.as_ref().map(|x| &x.0.slug)];
+                }
+            }
+            div.column {
+                label for="title" { "Post Title" }
+                input type="text" name="title" spellcheck="true" required="true" placeholder="The title of this post" value=[current.as_ref().map(|x| &x.0.title)];
+            }
+            div.column {
+                label for="published" { "Published" }
+                input type="checkbox" name="published" value="true" checked[current.as_ref().map(|x| x.0.published).unwrap_or_default()];
+            }
+        }
+        div.row {
+            div.column {
+                label for="date" { "Post Date" }
+                input type="date" name="date" required="true" value=[current.as_ref().map(|x| &x.0.date)];
+            }
+            div.column {
+                label for="content_type" { "Content Type" }
+                select name="content_type" required="true" {
+                    option value=(Markdown) selected[current.as_ref().map(|x| x.0.content_type == Markdown).unwrap_or_default()] { (Markdown) }
+                    option value=(RestructuredText) selected[current.as_ref().map(|x| x.0.content_type == RestructuredText).unwrap_or_default()] { (RestructuredText)}
+                }
+            }
+            div.column {
+                label for="labels" { "Labels" }
+                input type="text" name="labels" placeholder="label,label,label" value=[current.as_ref().map(|x| x.0.labels.join(","))];
+            }
+        }
+        div.row {
+            div.column {
+                label for="raw_content" { "Raw Content" }
+                textarea name="raw_content" spellcheck="true" wrap="soft" placeholder="Your post content here.." {
+                    @if let Some((_, c)) = current {
+                        (c)
+                    }
+                }
+                button type="submit" { "Submit" }
+            }
+        }
+    }
+}
+
+pub(crate) fn new_posts_page(post: Option<(Post, String)>, error: Option<String>, htmx_context: Option<HtmxContext>) -> Response {
+    render_body_html_or_htmx(StatusCode::OK, "New post", render_body_semantics("New Post", vec![html! {
+        @if let Some(e) = error {
+            div {
+                (e)
+            }
+        }
+        form action="/posts/new" method="post" {
+            (render_post_form(post, true))
+        }
+    }]), htmx_context)
+}
+
+pub(crate) fn edit_posts_page(post: Post, content: String, error: Option<String>, htmx_context: Option<HtmxContext>) -> Response {
+    render_body_html_or_htmx(StatusCode::OK, "Edit post", render_body_semantics("Edit Post", vec![html! {
+        @if let Some(e) = error {
+            div {
+                (e)
+            }
+        }
+        form action={ "/posts/" (post.slug) } method="post" {
+            (render_post_form(Some((post, content)), true))
+        }
+    }]), htmx_context)
+}
+
+pub(crate) fn debug_objects_page(objects: Vec<ObjectMeta>, htmx_context: Option<HtmxContext>) -> Response {
+    render_body_html_or_htmx(StatusCode::OK, "Debug", render_body_semantics("Debug", vec![
+        html! {
+            table {
+                thead {
+                    tr {
+                        th { "Location" }
+                        th { "Size" }
+                        th { "Last Modified" }
+                    }
+                }
+                tbody {
+                    @if objects.is_empty() {
+                        tr {
+                            td colspan="3" { "No objects" }
+                        }
+                    } @else {
+                        @for object in objects {
+                            tr {
+                                td { (object.location) }
+                                td { (object.size) }
+                                td { (object.last_modified) }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    ]), htmx_context)
+}
--- src/store.rs
+++ src/store.rs
@@ -1,6 +1,6 @@
 use std::fmt::Display;
 use crate::path_utils::path_tail;
-use anyhow::{anyhow, Error};
+use anyhow::{anyhow, Context, Error};
 use base64::prelude::BASE64_STANDARD_NO_PAD;
 use base64::Engine;
 use bytes::Bytes;
@@ -13,7 +13,7 @@ use image::ImageReader;
 use itertools::Itertools;
 use object_store::local::LocalFileSystem;
 use object_store::path::{Path, PathPart};
-use object_store::{ObjectStore, PutOptions, PutPayload};
+use object_store::{ObjectMeta, ObjectStore, PutOptions, PutPayload};
 use serde::{Deserialize, Serialize};
 use std::io::Cursor;
 use std::slice::Iter;
@@ -38,6 +38,7 @@ pub struct Post {
     pub slug: String,
     pub title: String,
     pub content_type: PostContentType,
+    pub published: bool,
     pub labels: Vec<String>,
 }
 
@@ -92,6 +93,7 @@ pub struct Store {
     sub_path: Path,
 }
 
+#[allow(dead_code)]
 impl Store {
     const MEDIUM_VARIANT_WIDTH: u32 = 1000;
     const MEDIUM_VARIANT_HEIGHT: u32 = 1000;
@@ -102,6 +104,7 @@ impl Store {
         Self { os, sub_path }
     }
 
+    #[allow(dead_code)]
     pub fn from_url(url: &Url) -> Result<Self, Error> {
         if url.scheme() != "file" {
             let opts = url.query_pairs()
@@ -116,14 +119,14 @@ impl Store {
         }
     }
 
-    pub async fn upsert_post(&self, post: &Post, raw: &[u8]) -> Result<(), Error> {
+    pub async fn upsert_post(&self, post: &Post, content: &str) -> Result<(), Error> {
         let post_path = self.sub_path.child("posts").child(post.slug.clone());
 
-        let post_meta = PostMetadata::V1((post.date, post.title.clone(), post.content_type.clone()));
+        let post_meta = PostMetadata::V1((post.date, post.title.clone(), post.content_type.clone(), post.published));
         let post_meta_bytes = postcard::to_allocvec(&post_meta)?;
         let post_meta_raw = BASE64_STANDARD_NO_PAD.encode(&post_meta_bytes);
 
-        self.os.put_opts(&post_path.child("content"), PutPayload::from(raw.to_vec()), PutOptions::default()).await?;
+        self.os.put_opts(&post_path.child("content"), PutPayload::from(content.to_string()), PutOptions::default()).await?;
         self.os.put_opts(&post_path.child("props").child(post_meta_raw.clone()), PutPayload::default(), PutOptions::default()).await?;
         // write the tags concurrently
         FuturesUnordered::from_iter(post.labels.iter().map(| lbl| {
@@ -137,20 +140,18 @@ impl Store {
         // and now clean up anything extra that we don't need
         let cleanup_paths = self.os.list(Some(&post_path))
             .map_ok(|m| m.location)
+            .boxed()
             .try_filter(|p| {
-                let mut parts = p.parts();
-                if let Some((sec, k)) = parts.next_tuple::<(PathPart, PathPart)>() {
-                    return ready((sec.as_ref() == "props" && k.as_ref() != post_meta_raw) ||
+                let tail = path_tail(p, &post_path);
+                if let Some((sec, k)) = tail.parts().next_tuple::<(PathPart, PathPart)>() {
+                    return ready((sec.as_ref() == "props" && k.as_ref() != post_meta_raw.as_str()) ||
                         (sec.as_ref() == "labels" && !post.labels.contains(&k.as_ref().to_string())))
                 }
                 ready(false)
             })
             .boxed();
-        if self.os.delete_stream(cleanup_paths).try_collect::<Vec<Path>>().await?.is_empty() {
-            Err(Error::msg("cleanup failed"))
-        } else {
-            Ok(())
-        }
+        self.os.delete_stream(cleanup_paths).try_collect::<Vec<Path>>().await?;
+        Ok(())
     }
 
     async fn delete_paths_by_prefix(&self, prefix: &Path) -> Result<(), Error> {
@@ -172,8 +173,7 @@ impl Store {
         let slug = format!("{}-{}", Local::now().format("%Y%m%dT%H%M%S"), pre_slug);
         let image_path = self.sub_path.child("images").child(slug.clone());
         let image_reader = ImageReader::new(Cursor::new(raw))
-            .with_guessed_format()
-            .expect("reader should not fail on buffered data");
+            .with_guessed_format().context("failed to guess format")?;
         let image = image_reader.decode()?;
         let medium = if image.width() > Self::MEDIUM_VARIANT_WIDTH || image.height() > Self::MEDIUM_VARIANT_HEIGHT {
             image.resize(Self::MEDIUM_VARIANT_WIDTH, Self::MEDIUM_VARIANT_HEIGHT, image::imageops::FilterType::Lanczos3)
@@ -183,15 +183,15 @@ impl Store {
         let thumbnail = image.thumbnail(Self::THUMB_VARIANT_WIDTH, Self::THUMB_VARIANT_HEIGHT);
 
         let mut original_data = vec![];
-        image.write_with_encoder(WebPEncoder::new_lossless(&mut original_data)).expect("image translation should never fail");
+        image.write_with_encoder(WebPEncoder::new_lossless(&mut original_data))?;
         self.os.put(&image_path.child("original.webp"), PutPayload::from(original_data)).await?;
 
         let mut medium_data = vec![];
-        medium.write_with_encoder(WebPEncoder::new_lossless(&mut medium_data)).expect("image translation should never fail");
+        medium.write_with_encoder(WebPEncoder::new_lossless(&mut medium_data))?;
         self.os.put(&image_path.child("medium.webp"), PutPayload::from(medium_data)).await?;
 
         let mut thumbnail_data = vec![];
-        thumbnail.write_with_encoder(WebPEncoder::new_lossless(&mut thumbnail_data)).expect("image translation should never fail");
+        thumbnail.write_with_encoder(WebPEncoder::new_lossless(&mut thumbnail_data))?;
         self.os.put(&image_path.child("thumbnail.webp"), PutPayload::from(thumbnail_data)).await?;
 
         Ok(slug)
@@ -204,7 +204,7 @@ impl Store {
     fn labels_from_paths(i: Iter<&Path>, offset: usize) -> Vec<String> {
         i.into_iter().filter_map(|p| {
             let mut iter = p.parts();
-            if iter.nth(offset + 2).map(|pp| pp.as_ref() == "labels").is_some() {
+            if iter.nth(offset + 2).filter(|pp| pp.as_ref() == "labels").is_some() {
                 iter.next().map(|pp| pp.as_ref().to_string())
             } else {
                 None
@@ -229,17 +229,19 @@ impl Store {
 
         // each path looks like images/... since we've removed the prefix path already
         Ok(objects_paths.iter()
-            .into_group_map_by(|f| f.parts().nth(1)).iter()
-            .filter(|(slug, _)| slug.is_some())
+            .into_group_map_by(|f| f.parts().nth(1))
+            .iter()
+            .flat_map(|(slug, paths)| slug.as_ref().map(|p| (p.as_ref(), paths))  )
             .map(|(slug, paths)| {
-                let slug = slug.as_ref().unwrap().as_ref().to_string();
+                let slug = slug.to_string();
                 let labels = Self::labels_from_paths(paths.iter(), 0);
                 match Self::props_part_from_paths(paths.iter(), 0) {
-                    Some(PostMetadata::V1((date, title, content_type))) => Post {
+                    Some(PostMetadata::V1((date, title, content_type, published))) => Post {
                         date,
                         slug,
                         title,
                         content_type,
+                        published,
                         labels,
                     },
                     None => Post {
@@ -270,11 +272,12 @@ impl Store {
         let post_paths_refs: Vec<&Path> = post_paths.iter().collect();
         let labels = Self::labels_from_paths(post_paths_refs.iter(), 0);
         let post = match Self::props_part_from_paths(post_paths_refs.iter(), 0) {
-            Some(PostMetadata::V1((date, title, content_type))) => Post {
+            Some(PostMetadata::V1((date, title, content_type, published))) => Post {
                 date,
                 slug: slug.to_string(),
                 title,
                 content_type,
+                published,
                 labels,
             },
             None => Post {
@@ -290,7 +293,8 @@ impl Store {
         Ok(self.os.list_with_delimiter(Some(&self.sub_path.child("images")))
             .await?
             .common_prefixes.iter()
-            .map(|m| path_tail(&Path::from(m.filename().unwrap()), &self.sub_path).to_string())
+            .flat_map(|m| m.filename().map(Path::from))
+            .map(|m| path_tail(&m, &self.sub_path).to_string())
             .collect_vec())
     }
 
@@ -306,6 +310,12 @@ impl Store {
             Err(e) => Err(e.into()),
         }
     }
+
+    pub async fn list_object_meta(&self) -> Result<Vec<ObjectMeta>, Error> {
+        Ok(self.os.list(None)
+            .boxed()
+            .try_collect::<Vec<ObjectMeta>>().await?)
+    }
 }
 
 impl Default for Store {
@@ -316,7 +326,7 @@ impl Default for Store {
 
 #[derive(Debug,Serialize,Deserialize,Clone,PartialEq,Eq,PartialOrd,Ord)]
 enum PostMetadata {
-    V1((NaiveDate, String, PostContentType)),
+    V1((NaiveDate, String, PostContentType, bool)),
 }
 
 impl TryFrom<PathPart<'_>> for PostMetadata {
@@ -330,8 +340,11 @@ impl TryFrom<PathPart<'_>> for PostMetadata {
 
 impl From<PostMetadata> for PathPart<'_> {
     fn from(meta: PostMetadata) -> Self {
-        let raw = postcard::to_allocvec(&meta).expect("Failed to serialize PostMetadata");
-        PathPart::from(BASE64_STANDARD_NO_PAD.encode(&raw))
+        if let Ok(raw) = postcard::to_allocvec(&meta) {
+            PathPart::from(BASE64_STANDARD_NO_PAD.encode(&raw))
+        } else {
+            PathPart::default()            
+        }
     }
 }
 
@@ -343,10 +356,10 @@ mod tests {
     use std::ops::Deref;
 
     #[test]
-    fn test_ser_der() {
-        let p = PostMetadata::V1((NaiveDate::from_ymd_opt(2024, 1, 2).unwrap(), "fizz".to_string(), PostContentType::Markdown));
-        let b = postcard::to_allocvec(&p).unwrap();
-        assert_eq!(b.len(), 18);
+    fn test_ser_der() -> Result<(), Error> {
+        let p = PostMetadata::V1((NaiveDate::from_ymd_opt(2024, 1, 2).unwrap_or_default(), "fizz".to_string(), PostContentType::Markdown, false));
+        let b = postcard::to_allocvec(&p)?;
+        assert_eq!(b.len(), 19);
         assert_eq!(b, vec![
             // enum 0
             0,
@@ -357,27 +370,32 @@ mod tests {
             4, 102, 105, 122, 122,
             // enum 0
             0,
+            // boolean 0
+            0,
         ]);
-        let p2 = postcard::from_bytes(b.as_slice()).unwrap();
+        let p2 = postcard::from_bytes(b.as_slice())?;
         assert_eq!(p, p2);
+        Ok(())
     }
 
     #[tokio::test]
-    async fn test_store_images_empty() {
+    async fn test_store_images_empty() -> Result<(), Error> {
         let store = Store::default();
-        assert!(store.list_images().await.unwrap().is_empty());
-        assert_eq!(store.get_image_raw("fizz", ImageVariant::Medium).await.unwrap(), None);
+        assert!(store.list_images().await?.is_empty());
+        assert_eq!(store.get_image_raw("fizz", ImageVariant::Medium).await?, None);
+        Ok(())
     }
 
     #[tokio::test]
-    async fn test_store_posts_empty() {
+    async fn test_store_posts_empty() -> Result<(), Error> {
         let store = Store::default();
-        assert!(store.list_posts().await.unwrap().is_empty());
-        assert_eq!(store.get_post_raw("fizz").await.unwrap(), None);
+        assert!(store.list_posts().await?.is_empty());
+        assert_eq!(store.get_post_raw("fizz").await?, None);
+        Ok(())
     }
 
     #[tokio::test]
-    async fn test_store_images() {
+    async fn test_store_images() -> Result<(), Error> {
         let store = Store {
             sub_path: Path::from("default"),
             ..Store::default()
@@ -385,19 +403,67 @@ mod tests {
 
         let eg_image = DynamicImage::new(100, 100, ColorType::Rgb8);
         let mut eg_data: Vec<u8> = vec![];
-        eg_image.write_with_encoder(JpegEncoder::new(&mut eg_data)).unwrap();
+        eg_image.write_with_encoder(JpegEncoder::new(&mut eg_data))?;
 
-        let slug = store.create_image("test", eg_data.deref()).await.unwrap();
-        assert_eq!(store.list_images().await.unwrap(), vec![slug.clone()]);
-        assert_ne!(store.get_image_raw(&slug, ImageVariant::Thumbnail).await.unwrap(), None);
-        assert_ne!(store.get_image_raw(&slug, ImageVariant::Medium).await.unwrap(), None);
-        assert_ne!(store.get_image_raw(&slug, ImageVariant::Original).await.unwrap(), None);
+        let slug = store.create_image("test", eg_data.deref()).await?;
+        assert_eq!(store.list_images().await?, vec![slug.clone()]);
+        assert_ne!(store.get_image_raw(&slug, ImageVariant::Thumbnail).await?, None);
+        assert_ne!(store.get_image_raw(&slug, ImageVariant::Medium).await?, None);
+        assert_ne!(store.get_image_raw(&slug, ImageVariant::Original).await?, None);
 
-        store.delete_image(&slug).await.unwrap();
-        assert_eq!(store.list_images().await.unwrap(), Vec::<String>::new());
-        assert_eq!(store.get_image_raw(&slug, ImageVariant::Thumbnail).await.unwrap(), None);
-        assert_eq!(store.get_image_raw(&slug, ImageVariant::Medium).await.unwrap(), None);
-        assert_eq!(store.get_image_raw(&slug, ImageVariant::Original).await.unwrap(), None);
+        store.delete_image(&slug).await?;
+        assert_eq!(store.list_images().await?, Vec::<String>::new());
+        assert_eq!(store.get_image_raw(&slug, ImageVariant::Thumbnail).await?, None);
+        assert_eq!(store.get_image_raw(&slug, ImageVariant::Medium).await?, None);
+        assert_eq!(store.get_image_raw(&slug, ImageVariant::Original).await?, None);
+        Ok(())
     }
 
+    #[tokio::test]
+    async fn test_store_posts() -> Result<(), Error> {
+        let store = Store {
+            sub_path: Path::from("default"),
+            ..Store::default()
+        };
+        store.upsert_post(&Post{
+            date: NaiveDate::from_ymd_opt(2020, 1, 1).ok_or(anyhow!("invalid date"))?,
+            slug: "my-first-post".to_string(),
+            title: "My first post".to_string(),
+            content_type: PostContentType::Markdown,
+            published: true,
+            labels: vec!["blue".to_string(), "green".to_string()],
+        }, "my-content").await?;
+
+        println!("{:#?}", store.list_object_meta().await?);
+
+        let (post, content) = store.get_post_raw("my-first-post").await?.unwrap_or_default();
+        assert_eq!(post.date, NaiveDate::from_ymd_opt(2020, 1, 1).ok_or(anyhow!("invalid date"))?);
+        assert_eq!(post.slug, "my-first-post");
+        assert_eq!(post.title, "My first post");
+        assert_eq!(post.content_type, PostContentType::Markdown);
+        assert!(post.published);
+        assert_eq!(post.labels, vec!["blue".to_string(), "green".to_string()]);
+        assert_eq!(content, "my-content".to_string());
+        assert_eq!(store.list_object_meta().await?.len(), 4);
+        store.upsert_post(&Post{
+            date: NaiveDate::from_ymd_opt(2020, 1, 2).ok_or(anyhow!("invalid date"))?,
+            slug: "my-first-post".to_string(),
+            title: "My updated first post".to_string(),
+            content_type: PostContentType::Markdown,
+            published: false,
+            labels: vec!["red".to_string(), "green".to_string()],
+        }, "my-updated-content").await?;
+
+        let (post, content) = store.get_post_raw("my-first-post").await?.unwrap_or_default();
+        assert_eq!(post.date, NaiveDate::from_ymd_opt(2020, 1, 2).ok_or(anyhow!("invalid date"))?);
+        assert_eq!(post.slug, "my-first-post");
+        assert_eq!(post.title, "My updated first post");
+        assert_eq!(post.content_type, PostContentType::Markdown);
+        assert!(!post.published);
+        assert_eq!(post.labels, vec!["green".to_string(), "red".to_string()]);
+        assert_eq!(content, "my-updated-content".to_string());
+        assert_eq!(store.list_object_meta().await?.len(), 4);
+
+        Ok(())
+    }
 }
